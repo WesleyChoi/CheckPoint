47686b6b4fc40a28bd1492e4691dea3a4b9df44b
WesleyChoi <57197649+WesleyChoi@users.noreply.github.com>
2020-04-28 14:31:51 -0700

    Update studentlogin.php

--- CheckPoint_v1.1/studentlogin.php
+++ CheckPoint_v1.1/studentlogin.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>CheckPoint</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
-</head>
+<?php
+    require "header.php";
+?>
 <body>
-	<div id="header"> 
-		<h1>CHECKPOINT</h1>
-    </div>
     <div id="frm" class="container">
         <form action="process.php" method="POST">
+            <text>STUDENT LOGIN</text>
             <p>
                 <label>Username </label>
                 <input type="text" id="user" name="user"    />
@@ -28,4 +20,6 @@
         </form>
     </div>
 </body>
-</html> 
+<?php
+    require "footer.php";
+?>
