f04aa1347ccd6f2b929c91f9bf5c7800ca31b609
WesleyChoi <57197649+WesleyChoi@users.noreply.github.com>
2020-04-28 14:16:11 -0700

    Create teacherlogin.php

---
+++ CheckPoint_v1.1/teacherlogin.php
@@ -0,0 +1,24 @@
+<?php
+    require "header.php";
+?>
+
+<body>
+    <div id="frm" class="container">
+        <form action="process.php" method="POST">
+            <text>TEACHER LOGIN</text>
+            <p>
+                <label>Username </label>
+                <input type="text" id="user" name="user"    />
+            </p>
+            <p>
+                <label>Password </label>
+                <input type="text" id="pass" name="pass"   />
+            </p>
+            <p>
+                <input type="submit" id="btn" value="Login"     />
+            </p>
+        <a href=studentlogin.php>Are you a student? Login here.</a>
+        </form>
+    </div>
+</body>
+</html> 
