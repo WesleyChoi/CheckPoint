8691e7327f18716fa77e05f5da43100ab5cbff79
k-chew <kailey.chew@uwaterloo.ca>
2020-04-30 13:26:58 -0700

    Worked on login files, updated stylesheet.

--- header.php
+++ header.php
@@ -8,5 +8,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
 </head>
 <div id="header"> 
-	<a href=index.php style="color: black; text-decoration: none;"><h1>CHECKPOINT</h1></a>
+	<a id="title" href=index.php style="color: black; text-decoration: none;"><h1>CHECKPOINT</h1></a>
+	<nav class="nav-header-main">
+		<ul>
+			<li><a href="#">About Us</a></li>
+			<li><a href="#">Feedback</a></li>
+		</ul>
+	</nav>
 </div>
--- index.php
+++ index.php
@@ -4,9 +4,12 @@
     <body>
         <a href="signup.php">Login/Signup</a>
         <div id="container">
-            <p>You are logged out!</p>
-            <p>You are logged in!</p>
+            <p class="login-status">You are logged out!</p>
+            <p class="login-status">You are logged in!</p>
         </div>
+        <form action="includes/logout.inc.php" method="post">
+            <button type="submit" name="logout-submit">Logout</button>
+        </form>
     </body>
 <?php
     require "footer.php";
