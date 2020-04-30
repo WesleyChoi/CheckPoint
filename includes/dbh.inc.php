7346831f7c753864a79e444bfa5fbbcdf5f92d41
Wesley <wesleyhchoi@gmail.com>
2020-04-30 14:07:50 -0700

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
-$dBName = "login";
+$dBName = "loginssystem";
 
 $conn = mysqli_connect($servername, $dBUsername $dBPassword, $dBName);
 
