500da0bab4e0147a0e1129e755a375b1f3265765
Wesley <wesleyhchoi@gmail.com>
2020-04-30 11:37:17 -0700

    Create studentlogin.php

---
+++ studentlogin.php
@@ -0,0 +1,25 @@
+<?php
+    require "header.php";
+?>
+<body>
+    <div id="frm" class="container">
+        <form action="process.php" method="POST">
+            <text>STUDENT LOGIN</text>
+            <p>
+                <label>Username </label>
+                <input type="text" id="user" name="user"    />
+            </p>
+            <p>
+                <label>Password </label>
+                <input type="text" id="pass" name="pass"   />
+            </p>
+            <p>
+                <input type="submit" id="btn" value="Login"     />
+            </p>
+        <a href=teacherlogin.php>Are you a teacher? Login here.</a>
+        </form>
+    </div>
+</body>
+<?php
+    require "footer.php";
+?>
