cdc725b7d679481e22ec534e0269cb83973c8f9c
Wesley <wesleyhchoi@gmail.com>
2020-04-30 13:48:11 -0700

    Update signup.inc.php

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -20,6 +20,43 @@ if(isset($_POST['signup=submit'])) {
 
         $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
         $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        }
+        else {
+            mysqli_smt_bind_param($stmt, "s", $username);
+            mysqli_stmt_execute($stmt);
+            mysqli_stmt_store_result($stmt);
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            if ($resultCheck > 0) {
+                header("Location: ../signup.php?error=usertaken");
+                exit();
+            }
+            else{
+                $sql = "INSERT INTO users (uidUsers, pwdUsers) VALUES (?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                }
+                else{
+                    $hashedpwd = password_hash($password, PASSWORD_DEFAULT)
 
+                    mysqli_smt_bind_param($stmt, "ss", $username, $password);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+
+                }
+            }
+        }
     }
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+    
+}
+else {
+    header("Location: ../signup.php");
+    exit();
 }
