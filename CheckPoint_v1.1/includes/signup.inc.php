d9795d0b5f4e07a4fb66fbf998caae908f5b44b4
Wesley <wesleyhchoi@gmail.com>
2020-04-30 11:34:18 -0700

    Update signup.inc.php

--- CheckPoint_v1.1/includes/signup.inc.php
+++ CheckPoint_v1.1/includes/signup.inc.php
@@ -2,6 +2,24 @@
 
 if(isset($_POST['signup=submit'])) {
 
+    require 'dbh.inc.php';
 
+    $username = $_POSTp['uid'];
+    $password = $_POSTp['pwd'];
+    $passwordrepeat = $_POSTp['pwd-repeat'];
     
+    if (empty($username) || empty($password) || empty($passwordrepeat)) {
+        header("Location: ../signup.php?error=emptyfields&uid=".$username);
+        exit();
+    }
+    else if ($password !== $passwordrepeat) {
+        header("Location: ../signup.php?error=passwordcheck&uid=".$username);
+        exit();
+    }
+    else {
+
+        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
+        $stmt = mysqli_stmt_init($conn);
+
+    }
 }
