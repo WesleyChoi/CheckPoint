3675f1e2ebee73b582639be447ae9ad39d069914
Wesley <wesleyhchoi@gmail.com>
2020-05-05 10:06:08 -0700

    Update feedback.inc.php

--- includes/feedback.inc.php
+++ includes/feedback.inc.php
@@ -1,7 +1,19 @@
 <?php
-$name = $_POST['name']
-$email = $_POST['email']
-$message = $_POST['message']
+
+    if (isset($_POST['submit'])) {
+        $user_name = $_POST['name'];
+        $email_subject = "New Form Submission";
+        $user_email = $_POST['email'];
+        $message = $_POST['message'];
+    
+        $to = 'wesleyhchoi@gmail.com';
+        $headers = "From: ".$email_from;
+        $txt = "You have received an email from ".$user_name.".\n\n".$message;
+
+
+        mail($to, $email_subject, $txt, $headers);
+        header("Location: index.php?mailsent");
+    }
 
 
 ?>
