2e2585790d23b2f1f8ccf74ba06d8e8540c6914d
Wesley <wesleyhchoi@gmail.com>
2020-05-04 23:35:57 -0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -3,30 +3,25 @@
 ?>
 
 <body>
-    <section id="container">
+    <div id="include">
         <h2>Learn more about us!</h2>
         <div id="wesley">
-            <h3>Wesley Choi</h3>
+            <h2>Wesley Choi</h2>
             <p>Hey! I'm Wesley and I'm second year undergraduate student at UBC currently pursuing a BSc in Computer Science. I'm super interested in software engineering, UX/UI, and graphic design. Feel free to have a look at my portfolio or to contact me!</p>
-        </div>
-        <div id="kailey">
-            <h3>Kailey Chew</h3>
-            <p>Insert blurb here</p>
-        </div>
-    </section>
-    <div class="container">
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    <div class="work_title  wow fadeInUp animated">
-                        <h1>Latest Works</h1>
-                        <img src="images/shape.png" alt="">
-                        <p>Some of my latest works. <br> Please refer to my portfolio to see more!
-                        <h3><a href="images/TFC_Portfolio.pdf" download>View Portolio</a></h3></br>
-                        </p>
-                    </div>
-                </div>
+            <div id="icons">
+                <a href="https://github.com/WesleyChoi"><img src="githubicon.png" height="50"></a>
+                <a href="https://www.linkedin.com/in/wesley-choi-697706196/"><img src="linkedinicon.png" height="52"></a>
             </div>
         </div>
+        <div id="kailey">
+            <h2>Kailey Chew</h2>
+            <p>Insert blurb here</p>
+            <div id="icons">
+                <a href="https://github.com/k-chew"><img src="githubicon.png" height="50"></a>
+                <a href="https://www.linkedin.com/in/kailey-chew-xx2a/"><img src="linkedinicon.png" height="52"></a>
+            </div>
+        </div>
+    </div>
 </body>
 
 <?php
