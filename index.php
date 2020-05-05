3420f12b346c323c49a85b8d6bf0c9fb484bf46e
Wesley <wesleyhchoi@gmail.com>
2020-05-04 22:07:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
                 }
             ?>
         </div>
+        <h2>Hello! Welcome to CheckPoint!</h2>
+        <p>With an estimated 88% of the workforce and 25-75% of college students being affected by procrastination, it seems that there is a prominent lack of organization and productivity in individuals’ day-to-day lives.</p>
+
+        <p><br>Our solution? Getting younger students more habituated with scheduling by creating a rewards system that incentivises the idea of keeping track of <b>what they need to get done.</b> 
+        <br>The goal is to <b>make productivity an integrated part of students' habits</b>, that will carry on past elementary or high school.</p>
+
     </body>
 <?php
     require "footer.php";
