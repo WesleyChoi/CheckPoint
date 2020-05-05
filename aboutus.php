e16ae7f0b9bd0f79208bb5926fce89a3651530a7
Wesley <wesleyhchoi@gmail.com>
2020-05-04 22:07:29 -0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -3,14 +3,30 @@
 ?>
 
 <body>
-    <div id="container">
+    <section id="container">
+        <h2>Learn more about us!</h2>
         <div id="wesley">
-            <p>Wesley Choi</p>
+            <h3>Wesley Choi</h3>
+            <p>Hey! I'm Wesley and I'm second year undergraduate student at UBC currently pursuing a BSc in Computer Science. I'm super interested in software engineering, UX/UI, and graphic design. Feel free to have a look at my portfolio or to contact me!</p>
         </div>
         <div id="kailey">
-            <p>Kailey Chew</p>
+            <h3>Kailey Chew</h3>
+            <p>Insert blurb here</p>
         </div>
-    </div>
+    </section>
+    <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="work_title  wow fadeInUp animated">
+                        <h1>Latest Works</h1>
+                        <img src="images/shape.png" alt="">
+                        <p>Some of my latest works. <br> Please refer to my portfolio to see more!
+                        <h3><a href="images/TFC_Portfolio.pdf" download>View Portolio</a></h3></br>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
 </body>
 
 <?php
