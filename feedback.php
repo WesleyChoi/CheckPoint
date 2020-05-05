539086baea8f85f0675dfe81c5e74f1c8d6e3a12
Wesley <wesleyhchoi@gmail.com>
2020-05-04 22:07:34 -0700

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -3,7 +3,26 @@
 ?>
 <body>
     <div id="container">
-        <t>Give us feedback!</t><br>
+        <t>Want to contact us, send feedback, or learn more? Feel free to fill out the form below!</t><br>
+    </div>
+    <div id="frm">
+        <form method="post">
+            <p>
+                <label>Name </label>
+                <input type="text" name="uidfeedback"    />
+            </p>
+            <p>
+                <label>Email </label>
+                <input type="text" name="emailfeedback"    />
+            </p>
+            <p>
+                <label>Feedback </label>
+                <textarea id="feedback" name="feedback" placeholder="Write Feedback..."></textarea>
+            </p>
+            <p>
+                <button type="submit" name="Submit-Feedback">Submit Feedback</button>
+            </p>
+        </form>
     </div>
 </body>
 <?php
