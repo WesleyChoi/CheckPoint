49620460a12aeab6dca2524c280e15bacfb7d4e5
k-chew <kailey.chew@uwaterloo.ca>
2020-05-05 11:47:03 -0700

    Updated aboutus.

--- aboutus.php
+++ aboutus.php
@@ -5,20 +5,21 @@
 <body>
     <div id="include">
         <h2>Learn more about us!</h2>
-        <div id="wesley">
+        <div id="contact">
             <h2>Wesley Choi</h2>
             <p>Hey! I'm Wesley and I'm second year undergraduate student at UBC currently pursuing a BSc in Computer Science. I'm super interested in software engineering, UX/UI, and graphic design. Feel free to have a look at my portfolio or to contact me!</p>
             <div id="icons">
-                <a href="https://github.com/WesleyChoi"><img src="githubicon.png" height="50"></a>
-                <a href="https://www.linkedin.com/in/wesley-choi-697706196/"><img src="linkedinicon.png" height="52"></a>
+                <a href="https://github.com/WesleyChoi" target="_blank"><img src="githubicon.png" height="50"></a>
+                <a href="https://www.linkedin.com/in/wesley-choi-697706196/" target="_blank"><img src="linkedinicon.png" height="52"></a>
             </div>
         </div>
-        <div id="kailey">
+        <div id="contact">
             <h2>Kailey Chew</h2>
-            <p>Insert blurb here</p>
+            <p>Hi there! Thanks for popping by.</p>
+            <p> I'm in my second year of Computer Science at the University of Waterloo, and I'm eager to gain experience in full-stack developing. Take a gander at my links if you like!</p>
             <div id="icons">
-                <a href="https://github.com/k-chew"><img src="githubicon.png" height="50"></a>
-                <a href="https://www.linkedin.com/in/kailey-chew-xx2a/"><img src="linkedinicon.png" height="52"></a>
+                <a href="https://github.com/k-chew" target="_blank"><img src="githubicon.png" height="50"></a>
+                <a href="https://www.linkedin.com/in/kailey-chew-xx2a/" target="_blank"><img src="linkedinicon.png" height="52"></a>
             </div>
         </div>
     </div>
