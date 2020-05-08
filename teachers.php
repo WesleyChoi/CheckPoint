206b89a9fdba7c6cc048a29d7dedb0a49b7477a5
Wesley <wesleyhchoi@gmail.com>
2020-05-08 07:44:51 -0700

    Update teachers.php

--- teachers.php
+++ teachers.php
@@ -1,7 +1,32 @@
 <?php
     require "header.php";
 ?>
+<?php 
+    // initialize errors variable
+	$errors = "";
 
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "todo");
+
+	// insert a quote if submit button is clicked
+	if (isset($_POST['submit'])) {
+		if (empty($_POST['task'])) {
+			$errors = "You must fill in the task";
+		}else{
+			$task = $_POST['task'];
+			$sql = "INSERT INTO tasks (task) VALUES ('$task')";
+			mysqli_query($db, $sql);
+			header('location: teachers.php');
+		}
+    }	
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+    
+        mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+        header('location: teachers.php');
+    }
+    
+?>
 <body>
     <div id="announcement">
         <t>This is the home page for teachers to access.</t><br>
@@ -15,46 +40,41 @@
         ?>
     </div> 
     <div id="frm">
-        <form method="post" action="frmphp/teachers.inc.php" class="input_form">
-            <p>
-                <label>Task Name </label>
-                <input type="text" name="taskname" class="task_input" required>
-            </p>
-            <p>
-                <label>Task Description </label>
-                <input type="text" name="taskabout" class="task_about" required>
-            </p>
-            <p>
-                <label>Task Value </label>
-                <input type="number" name="taskvalue" class="task_value" required>
-            </p>
+        <form method="post" action="teachers.php" class="input_form">
+            <?php if (isset($errors)) { ?>
+                <p><?php echo $errors; ?></p>
+            <?php } ?>
+
+            <input type="text" name="task" class="task_input">
             <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+        </form>
     </div>
     <div id="tasklist">
-            <table>
-                <thread>
+        <table>
+            <thead>
+                <tr>
+                    <th>N</th>
+                    <th>Tasks</th>
+                    <th style="width: 160px;">Delete Task</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php 
+                // select all tasks if page is visited or refreshed
+                $tasks = mysqli_query($db, "SELECT * FROM tasks");
+
+                $i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
                     <tr>
-                        <th>Tasks</th>
-                        <th>Description</th>
-                        <th>Task Value</th>
+                        <td> <?php echo $i; ?> </td>
+                        <td class="task"> <?php echo $row['task']; ?> </td>
+                        <td class="delete"> 
+                            <a href="teachers.php?del_task=<?php echo $row['id'] ?>">x</a> 
+                        </td>
                     </tr>
-                </thread>
-
-                <tbody>
-                    <?php
-
-                        $tasks = mysqli_query($conn, "SELECT * FROM tasks");
-
-                        $i = 1; while($row = mysqli_fetch_array($tasks)) { ?>
-                            <td class="task_input"> <?php echo $row['taskname']; ?> </td>
-                            <td class="task_about"> <?php echo $row['taskabout']; ?> </td>
-                            <td class="task_value"> <?php echo $row['taskvalue']; ?> </td>
-                            <td class="delete"> 
-                                <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
-                            </td>
-                            <?php $i++; } ?>
-                </tbody>
-            </table>
+                <?php $i++; } ?>	
+            </tbody>
+        </table>
     </div>
 </body>
 
