d41a45809112376ce56ef18f30c12044661b940a
Wesley <wesleyhchoi@gmail.com>
2020-05-08 16:09:06 -0700

    Co-authored-by: k-chew <kailey.chew@uwaterloo.ca>

--- teachers.php
+++ teachers.php
@@ -87,7 +87,7 @@
                 $stmt = mysqli_stmt_init($conn);
                 mysqli_stmt_execute($stmt);
                 $result = mysqli_stmt_get_result($stmt);
-                
+
                 $i = 1; while ($row = mysqli_fetch_array($result)) { ?>
                     <tr>
                         <td> <?php echo $i; ?> </td>
