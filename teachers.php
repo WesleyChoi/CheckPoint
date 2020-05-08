a2a360885fd3508b08d9a75e6c172aef708118c9
Wesley <wesleyhchoi@gmail.com>
2020-05-07 21:50:26 -0700

    Update teachers.php

--- teachers.php
+++ teachers.php
@@ -1,9 +1,10 @@
 <?php
     require "header.php";
 ?>
+
 <body>
     <div id="announcement">
-        <t>This is the home page for teachers to access</t><br>
+        <t>This is the home page for teachers to access.</t><br>
         <?php
             if (isset($_SESSION['userUid'])) {
                 echo 'Welcome to Checkpoint, ' . $_SESSION['userFn'];
@@ -13,13 +14,24 @@
             }
         ?>
     </div> 
-    <div id="container">
-        <h1>Your Classrooms</h1>
-        <button>Add a task</button>
-        <button>Delete task</button>
-        <button>View students' points</button>
+    <div id="frm">
+        <form method="post" action="frmphp/teachers.inc.php" class="input_form">
+            <p>
+                <label>Task Name </label>
+                <input type="text" name="taskname" class="task_input" required>
+            </p>
+            <p>
+                <label>Task Description </label>
+            <input type="text" name="taskabout" class="task_about" required>
+            </p>
+            <p>
+                <label>Task Value </label>
+            <input type="number" name="taskvalue" class="task_value" required>
+            </p>
+            <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
     </div>
 </body>
+
 <?php
     require "footer.php";
 ?>
