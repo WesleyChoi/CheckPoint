ac3b94524c71d4fd75431d4c76bb0c70e60a0280
Wesley <wesleyhchoi@gmail.com>
2020-05-07 22:52:39 -0700

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -9,8 +9,8 @@
             <h2>Wesley Choi</h2>
             <p>Hey! I'm Wesley and I'm second year undergraduate student at UBC currently pursuing a BSc in Computer Science. I'm super interested in software engineering, UX/UI, and graphic design. Feel free to have a look at my portfolio or to contact me!</p>
             <div id="icons">
-                <a href="https://github.com/WesleyChoi" target="_blank"><img src="githubicon.png" height="50"></a>
-                <a href="https://www.linkedin.com/in/wesley-choi-697706196/" target="_blank"><img src="linkedinicon.png" height="52"></a>
+                <a href="https://github.com/WesleyChoi" target="_blank"><img src="images/githubicon.png" height="50"></a>
+                <a href="https://www.linkedin.com/in/wesley-choi-697706196/" target="_blank"><img src="images/linkedinicon.png" height="52"></a>
             </div>
         </div>
         <div id="contact">
@@ -18,8 +18,8 @@
             <p>Hi there! Thanks for popping by.</p>
             <p> I'm in my second year of Computer Science at the University of Waterloo, and I'm eager to gain experience in full-stack developing. Take a gander at my links if you like!</p>
             <div id="icons">
-                <a href="https://github.com/k-chew" target="_blank"><img src="githubicon.png" height="50"></a>
-                <a href="https://www.linkedin.com/in/kailey-chew-xx2a/" target="_blank"><img src="linkedinicon.png" height="52"></a>
+                <a href="https://github.com/k-chew" target="_blank"><img src="images/githubicon.png" height="50"></a>
+                <a href="https://www.linkedin.com/in/kailey-chew-xx2a/" target="_blank"><img src="images/linkedinicon.png" height="52"></a>
             </div>
         </div>
     </div>
