29748e60d571e36f3bf4430a14ac0f4abdcc14f8
Wesley <wesleyhchoi@gmail.com>
2020-05-07 22:42:04 -0700

    Update teachers.inc.php

--- frmphp/teachers.inc.php
+++ frmphp/teachers.inc.php
@@ -10,23 +10,19 @@
     $conn = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
     if (isset($_POST['submit'])) {
-        if (empty($_POST['taskname']) {
-            $errors = "You must fill in the task";
-        }
-        if (empty($_POST['taskabout']) {
-            $errors = "You must fill in the task description";
-        }
-        if (empty($_POST['taskvalue']) {
-            $errors = "You must fill in the task description";
-        }
-        else {
-            $taskname = $_POST['taskname'];
-            $taskabout = $_POST['taskabout'];
-            $taskvalue = $_POST['taskvalue']
-            $sql = "INSERT INTO taskname (task) VALUES ('$taskname')";
-            mysqli_query($conn, $sql)
-            header('location: teachers.php')
-        }
+        $taskname = $_POST['taskname'];
+        $taskabout = $_POST['taskabout'];
+        $taskvalue = $_POST['taskvalue']
+        $sql = "INSERT INTO tasks (taskname, taskabout, taskvalue) VALUES (?, ?, ?)";
+        mysqli_query($conn, $sql)
+        header('location: teachers.php')
+    }
+
+    if (isset($_GET['del_task'])) {
+        $id = $_GET['del_task'];
+    
+        mysqli_query($db, "DELETE FROM tasks WHERE id=".$id);
+        header('location: index.php');
     }
 
 ?>
