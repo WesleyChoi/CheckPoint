6542a3137b1d9f0c1702778aed858ebaa3c1dd79
Wesley <wesleyhchoi@gmail.com>
2020-05-08 14:58:24 -0700

    Update students.php

--- students.php
+++ students.php
@@ -1,12 +1,20 @@
 <?php
     require "header.php";
 ?>
+<?php 
+    // initialize errors variable
+	$errors = "";
+
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "todo");
+?>
+
 <body>
     <div id="announcement">
         <t>This is the home page for students to access</t><br>
         <?php
             if (isset($_SESSION['userUid'])) {
-                echo 'Welcome to Checkpoint, ' . $_SESSION['userFn'];
+                echo 'Welcome to Checkpoint, ' . $_SESSION['userFn'] . '!<br>';
                 echo 'We wish you the best of luck in '.$_SESSION['courseName'].'!';
             }
             else {
@@ -27,7 +35,7 @@
                 <tbody>
                     <?php
 
-                        $tasks = mysqli_query($conn, "SELECT * FROM tasks");
+                        $tasks = mysqli_query($db, "SELECT * FROM tasks");
 
                         $i = 1; while($row = mysqli_fetch_array($tasks)) { ?>
                             <td class="task_input"> <?php echo $row['taskname']; ?> </td>
