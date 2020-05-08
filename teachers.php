b057ff1053bef637fbb0c2651954debd97f24cd7
Wesley <wesleyhchoi@gmail.com>
2020-05-07 22:42:10 -0700

    Update teachers.php

--- teachers.php
+++ teachers.php
@@ -22,13 +22,39 @@
             </p>
             <p>
                 <label>Task Description </label>
-            <input type="text" name="taskabout" class="task_about" required>
+                <input type="text" name="taskabout" class="task_about" required>
             </p>
             <p>
                 <label>Task Value </label>
-            <input type="number" name="taskvalue" class="task_value" required>
+                <input type="number" name="taskvalue" class="task_value" required>
             </p>
             <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+    </div>
+    <div id="tasklist">
+            <table>
+                <thread>
+                    <tr>
+                        <th>Tasks</th>
+                        <th>Description</th>
+                        <th>Task Value</th>
+                    </tr>
+                </thread>
+
+                <tbody>
+                    <?php
+
+                        $tasks = mysqli_query($conn, "SELECT * FROM tasks");
+
+                        $i = 1; while($row = mysqli_fetch_array($tasks)) { ?>
+                            <td class="task_input"> <?php echo $row['taskname']; ?> </td>
+                            <td class="task_about"> <?php echo $row['taskabout']; ?> </td>
+                            <td class="task_value"> <?php echo $row['taskvalue']; ?> </td>
+                            <td class="delete"> 
+                                <a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+                            </td>
+                            <?php $i++; } ?>
+                </tbody>
+            </table>
     </div>
 </body>
 
