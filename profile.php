18eea49e4594f2c36aed3c388a4ca9bdc1e89db3
k-chew <kailey.chew@uwaterloo.ca>
2020-05-07 21:21:49 -0700

    Displays student/teacher type.

--- profile.php
+++ profile.php
@@ -5,6 +5,7 @@
         <div id="container">
             <h1>Profile<h1>
             <?php echo $_SESSION['userFn'].' '.$_SESSION['userLn']; ?>
+            <?php echo $_SESSION['acctype']; ?>
         </div>
     </body>
 <?php
