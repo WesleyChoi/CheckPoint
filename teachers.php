3cec68979e7d14c05c9810ebf465b92de7183816
k-chew <kailey.chew@uwaterloo.ca>
2020-05-08 16:49:53 -0700

    Fixed task list and deletion.

--- teachers.php
+++ teachers.php
@@ -2,40 +2,41 @@
     require "header.php";
     require "includes/dbh.inc.php";
 
-    // initialize errors variable
-	$errors = "";
-
-	/* connect to database
-	conn = mysqli_connect("localhost", "root", "", "testtodo");*/
-
-	// insert a quote if submit button is clicked
+	// if form was submitted, insert task into database
 	if (isset($_POST['submit'])) {
-		if (empty($_POST['task'])) {
-			$errors = "You must fill in the task";
-		}else{
-            $task = $_POST['task'];
-            $taskabout = $_POST['taskabout'];
-            $taskvalue = $_POST['taskvalue'];
-			$sql = "INSERT INTO tasks (task, taskabout, taskvalue) VALUES (?, ?, ?)";
-            //mysqli_query($conn, $sql);
-            $stmt = mysqli_stmt_init($conn); // new sqli statement
-            // check that works with database
-            if (!mysqli_stmt_prepare($stmt, $sql)) {
-                header("Location: teachers.php?error=sqlerror");
-                exit();
-            }
-            else {
-                mysqli_stmt_bind_param($stmt, "ssi", $task, $taskabout, $taskvalue);
-                mysqli_stmt_execute($stmt);
-            }
-			header('Location: teachers.php');
-		}
+        $task = $_POST['task'];
+        $taskabout = $_POST['taskabout'];
+        $taskvalue = $_POST['taskvalue'];
+		$sql = "INSERT INTO tasks (classId, task, taskabout, taskvalue) VALUES (?, ?, ?, ?)";
+        //mysqli_query($conn, $sql);
+        $stmt = mysqli_stmt_init($conn); // new sqli statement
+        // check that works with database
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: teachers.php?error=sqlerror");
+            exit();
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, "issi", $_SESSION['classId'], $task, $taskabout, $taskvalue);
+            mysqli_stmt_execute($stmt);
+        }
+		header('Location: teachers.php');
     }	
+
+    // delete task
     if (isset($_GET['del_task'])) {
         $task_del = $_GET['del_task'];
-    
-        mysqli_query($conn, "DELETE FROM tasks WHERE task=".$task_del);
-        header('Location: teachers.php');
+        
+        $sql = "DELETE FROM tasks WHERE task=?";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header("Location: ../teachers.php?error=sqlerror");
+            exit();
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, "s", $task_del);
+            mysqli_stmt_execute($stmt);
+        }
+        header('Location: teachers.php?task_deleted');
     }
 ?>
 
@@ -62,9 +63,9 @@
                 }
             ?>
 
-            <input type="text" name="task" class="task_input" placeholder="Task Name"><br>
+            <input type="text" name="task" class="task_input" placeholder="Task Name" required><br>
             <input type="text" name="taskabout" class="task_about" placeholder="Task Description"><br>
-            <input type="text" name="taskvalue" class="task_value" placeholder="Worth How Many Points"><br>
+            <input type="text" name="taskvalue" class="task_value" placeholder="Worth How Many Points" required><br>
             <button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
         </form>
     </div>
@@ -75,30 +76,39 @@
         <table>
             <thead>
                 <tr>
-                    <th id="itemclmn">N</th>
-                    <th id="itemclmn">Tasks</th>
+                    <th id="itemclmn">Number</th>
+                    <th id="itemclmn">Task Name</th>
                     <th id="itemclmn">Task Description</th>
-                    <th id="itemclmn">Task Value</th>
+                    <th id="itemclmn">Point Value</th>
                     <th id="itemclmn">Delete Task</th>
                 </tr>
             </thead>
 
             <tbody>
                 <?php 
-                $sql = "SELECT * FROM tasks";
+                $sql = "SELECT * FROM tasks WHERE classId=?;";
                 $stmt = mysqli_stmt_init($conn);
-                mysqli_stmt_execute($stmt);
-                $result = mysqli_stmt_get_result($stmt);
+                if (!mysqli_stmt_prepare($stmt, $sql)) {
+                    header("Location: ../teachers.php?error=sqlerror");
+                    exit();
+                }
+                else {
+                    mysqli_stmt_bind_param($stmt, "i", $_SESSION['classId']);
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);
+                }
 
-                $i = 1; while ($row = mysqli_fetch_array($result)) { ?>
+                    $i = 1; while ($row = mysqli_fetch_array($result)) { ?>
                     <tr>
                         <td> <?php echo $i; ?> </td>
                         <td class="task"> <?php echo $row['task']; ?> </td>
+                        <td class="task"> <?php echo $row['taskabout']; ?> </td>
+                        <td class="task"> <?php echo $row['taskvalue']; ?> </td>
                         <td class="delete"> 
                             <a href="teachers.php?del_task=<?php echo $row['task'] ?>">x</a> 
                         </td>
                     </tr>
-                <?php $i++; } ?>
+                    <?php $i++; } ?>
             </tbody>
         </table>
     </div>
