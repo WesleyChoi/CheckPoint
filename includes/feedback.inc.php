0866909279cd03229fe5055ab95d94501720b3b5
k-chew <kailey.chew@uwaterloo.ca>
2020-05-08 13:04:20 -0700

    Changed email to uwaterloo.ca.

--- includes/feedback.inc.php
+++ includes/feedback.inc.php
@@ -5,13 +5,14 @@
         $user_email = $_POST['email'];
         $message = $_POST['message'];
     
-        $to = 'chewkailey@gmail.com';
+        $to = 'k2chew@uwaterloo.ca';
         $email_subject = "New Feedback Form Submission";
         $txt = "You have received an email from ".$user_name.".\n\n".$message;
         $headers = "From: ".$user_email;
 
         mail($to, $email_subject, $txt, $headers);
         header("Location: ../index.php?mailsent");
+        exit();
     }
 
 
