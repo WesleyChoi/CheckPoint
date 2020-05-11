61819f05e65470120026f35140928ca7bf49dcf6
Wesley <wesleyhchoi@gmail.com>
2020-05-11 00:10:32 -0700

    updated style for gstarted

--- gstarted.php
+++ gstarted.php
@@ -6,16 +6,25 @@
     <div id="include">
         <h1>Getting started with CheckPoint</h1>
 
-        <h3>Students</h3>
-        <ul>Make an account by navigating <a href="signup.php">here.</a> Make sure to have your class ID handy!
-        <ul>Once you're logged in, you should be able to view all the tasks that your teacher assigned.
-        <ul>After doing each task, make sure to check it off to complete it!
+        <div id="gstarted">
+            <h3 style="text-align: center">STUDENTS</h3>
+            <ul>
+                <li>Make an account by navigating <a href="signup.php">here.</a> Make sure to have your class ID handy!
+                <li>Once you're logged in, you should be able to view all the tasks that your teacher assigned.
+                <li>After doing each task, make sure to check it off to complete it!
+            </ul>
+        </div>
 
-        <h3>Teachers</h3>
-        <ul>Make an account by navigating <a href="signup.php">here.</a> Enter the name for your class and a class ID will be created. Make sure to jot this down somewhere and share it to your students!
-        <ul>Once you're logged in, you should be able to add a task: make sure to include it's title, a short description, and how many points it's worth.
-        <ul>Make sure to log out once you're done assigning all your tasks!
+        <div id="gstarted">
+            <h3 style="text-align: center">TEACHERS</h3>
+            <ul>
+                <li>Make an account by navigating <a href="signup.php">here.</a> Enter the name for your class and a class ID will be created. Make sure to jot this down somewhere and share it to your students!
+                <li>Once you're logged in, you should be able to add a task: make sure to include it's title, a short description, and how many points it's worth.
+                <li>Make sure to log out once you're done assigning all your tasks!
+            </ul>
+        </div>
 
+    </div>
 </body>
 
 <?php
