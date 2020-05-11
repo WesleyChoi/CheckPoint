570441d4b113e180e590bb21f258a9e6155e5c04
Wesley <wesleyhchoi@gmail.com>
2020-05-10 23:46:33 -0700

    updated gstarted and style sheet organization

--- gstarted.php
+++ gstarted.php
@@ -7,9 +7,14 @@
         <h1>Getting started with CheckPoint</h1>
 
         <h3>Students</h3>
+        <ul>Make an account by navigating <a href="signup.php">here.</a> Make sure to have your class ID handy!
+        <ul>Once you're logged in, you should be able to view all the tasks that your teacher assigned.
+        <ul>After doing each task, make sure to check it off to complete it!
 
         <h3>Teachers</h3>
-        <p>If you're a teacher, </p>
+        <ul>Make an account by navigating <a href="signup.php">here.</a> Enter the name for your class and a class ID will be created. Make sure to jot this down somewhere and share it to your students!
+        <ul>Once you're logged in, you should be able to add a task: make sure to include it's title, a short description, and how many points it's worth.
+        <ul>Make sure to log out once you're done assigning all your tasks!
 
 </body>
 
