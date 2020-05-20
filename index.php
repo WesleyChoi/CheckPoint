9349374c3319d470e1dd8d02cb8243478102e805
Kailey Chew <k2chew@cpu23.student.math>
2020-05-20 19:40:41 -0400

    Added "in progress".

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
         ?>
         <div id=intro>
             <h2>Hello! Welcome to CheckPoint!</h2>
-            <p>With an estimated 88% of the workforce and 25-75% of college students being affected by procrastination, it seems that there is a prominent lack of organization and productivity in individuals’ day-to-day lives.</p>
+	<h2> This website is in progress. Stay tuned for cool additions!</h2>    
+<p>With an estimated 88% of the workforce and 25-75% of college students being affected by procrastination, it seems that there is a prominent lack of organization and productivity in individuals’ day-to-day lives.</p>
 
             <p><br>Our solution? Getting younger students more habituated with scheduling by creating a rewards system that incentivises the idea of keeping track of <b>what they need to get done.</b> 
             <br>The goal is to <b>make productivity an integrated part of students' habits</b>, that will carry on past elementary or high school.</p>
