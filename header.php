06e075600b85d3e66e3a1c0483d1b7077862b272
Kailey Chew <k2chew@cpu19.student.math>
2020-05-26 13:52:41 -0400

    Fixed header file.

--- header.php
+++ header.php
@@ -12,37 +12,6 @@
 	<link href="https://fonts.googleapis.com/css2?family=Barlow&family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
 	<link rel='shortcut icon' href='images/favicon.png' type='image/png'>
 </head>
-<body>
-	<div id="header">
-		<div id="website-title">
-			<a href=index.php><img src="images/favicon.png" style="width: 40px; height: 40px; margin; 20px"></a>
-			<a href=index.php style="text-decoration: none;"><h1 style="text-align: left; display: inline; padding: 2px;">CHECKPOINT</h1></a>
-		</div>
-		<nav class="nav-header-main">
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<div class="dropdown">
-				<li><a>ABOUT</a></li>
-				<div class="dropdown-content">
-					<ul>
-					<li><a href="gstarted.php">Getting Started</a></li>
-					<li><a href="aboutus.php">About Us</a></li>
-					<li><a href="feedback.php">Feedback</a></li>
-					</ul>
-				</div>
-				</div>
-				<?php
-					if (isset($_SESSION['userUid'])) {
-						if ($_SESSION['accType'] = "student") {
-							echo '<li><a href="students.php">Lists</a></li>';
-						} else {
-							echo '<li><a href="teachers.php">Lists</a></li>';
-						}
-						echo '<li><a href="profile.php">'.$_SESSION['userFn'].'</a></li>';
-						echo '<li><a href="includes/logout.inc.php">Logout</a></li>';
-					}
-					else {
-						echo '<li><a href="login.php">Login</a></li>';
 <div id="header">
 	<div id="website-title">
 		<a href=index.php style="width: 0%; margin: 0 auto;"><img src="images/favicon.png" style="float: left; padding: 20px 0px 20px 20px; width: 60px; height: 60px;"></a>
@@ -68,8 +37,13 @@
 					} else {
 						echo '<li><a href="teachers.php">Lists</a></li>';
 					}
-				?>
-			</ul>
-		</nav>
-	</div>
-</body>
+					echo '<li><a href="profile.php">'.$_SESSION['userFn'].'</a></li>';
+					echo '<li><a href="includes/logout.inc.php">Logout</a></li>';
+				}
+				else {
+					echo '<li><a href="login.php">Login</a></li>';
+				}
+			?>
+		</ul>
+	</nav>
+</div>
