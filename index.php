b26268c49c0faa932a86c9fc98089cdbd0077efc
Kailey Chew <k2chew@cpu19.student.math>
2020-05-26 14:23:16 -0400

    Added textbox class.

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	    <div id="announcement">
 	    <h2> This website is in progress. There is more to come!</h2>    
 	    </div>
-	    <div id="container" style="font-size: 13px;">
+	    <div id="textbox" style="font-size: 13px;">
 		<p>With an estimated 88% of the workforce and 25-75% of college students being affected by procrastination, it seems that there is a prominent lack of organization and productivity in individuals’ day-to-day lives.</p>
             <p><br>Our solution? Getting younger students more habituated with scheduling by creating a rewards system that incentivises the idea of keeping track of <b>what they need to get done.</b> 
             <br>The goal is to <b>make productivity an integrated part of students' habits</b>, that will carry on past elementary or high school.</p>
