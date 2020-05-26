8ff2a402382e922f7aab46bbca83af994f91c646
Kailey Chew <k2chew@cpu19.student.math>
2020-05-26 13:34:06 -0400

    Fixed website title.

--- header.php
+++ header.php
@@ -9,13 +9,13 @@
 	<title>CheckPoint</title>
     <link href="style.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css2?family=Barlow&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Barlow&family=Roboto+Mono:wght@500&display=swap" rel="stylesheet">
 	<link rel='shortcut icon' href='images/favicon.png' type='image/png'>
 </head>
 <div id="header">
 	<div id="website-title">
-		<a href=index.php><img src="images/favicon.png" style="width: 40px; height: 40px;"></a>
-		<a href=index.php style="text-decoration: none;"><h1 style="text-align: left; display: inline; padding: 2px;">CHECKPOINT</h1></a>
+		<a href=index.php style="width: 0%; margin: 0 auto;"><img src="images/favicon.png" style="float: left; padding: 20px 0px 20px 20px; width: 60px; height: 60px;"></a>
+		<a href=index.php style="text-decoration: none; font-family: 'Roboto Mono', monospace;"><h1 style="float: right; color: black; padding: 20px 20px 20px 5px; font-size: 45px; margin: 0 auto;">CheckPoint</h1></a>
 	</div>
 	<nav class="nav-header-main">
 		<ul>
